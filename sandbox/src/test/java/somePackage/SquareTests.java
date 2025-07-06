2ea64f58ef10c6cec87e7b30830235a7359c60db
Dzmitry Shablinski <tassap2002@gmail.com>
2025-07-06 23:32:39 +0300

    попытки настроить gradle и testng

--- sandbox/src/test/java/somePackage/SquareTests.java
+++ sandbox/src/test/java/somePackage/SquareTests.java
@@ -6,6 +6,6 @@ public class SquareTests {
     @Test
     public void testArea() {
         Square square = new Square(5);
-        assert square.area() == 25;   // не путать == (сравнение) с = (присваивание)
+        assert square.area() == 25;
     }
 }
