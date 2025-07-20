6b52c200966caec6835cb4e9492f13a3b14851ea
Dzmitry Shablinski <tassap2002@gmail.com>
2025-07-20 16:08:16 +0300

    эксперименты с функциями

--- sandbox/src/main/java/Geometry.java
+++ sandbox/src/main/java/Geometry.java
@@ -1,9 +1,24 @@
 public class Geometry {
+
     public static void main(String[] args) {
-        System.out.println("Площадь квадрата со стороной " + 2 + " = " + (2*2));
-        var squareSide = 7.;
-        // var - переменная, а тип double
-        //чтобы показать джаве, что число должно быть не int, а double, можно написать 7.0 или просто 7.
-        System.out.println("Площадь квадрата со стороной " + squareSide + " = " + (squareSide * squareSide));
+        printSquareArea(5);
+        printRectangleArea(3,4);
+    }
+
+    private static void printRectangleArea(double firstSide, double secondSide) {
+        System.out.println("Площадь прямоугольника со сторонами " + firstSide + " и " + secondSide + " = " + getRectangleArea(firstSide, secondSide));
+    }
+
+    private static double getRectangleArea(double firstSide, double secondSide) {
+        return firstSide * secondSide;
+    }
+
+    static void printSquareArea(double side) {      // для параметров нужно явное указание типа, var уже не подойдет
+                                                    // static - означает, что ф-ция глобальная
+        System.out.println("Площадь квадрата со стороной " + side + " = " + getSquareArea(side));
+    }
+
+    private static double getSquareArea(double side) {
+        return side * side;
     }
 }
