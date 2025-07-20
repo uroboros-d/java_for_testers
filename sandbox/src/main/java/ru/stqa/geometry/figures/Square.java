20042a8182d0e58fe33bf9dbd62f7e303c726006
Dzmitry Shablinski <tassap2002@gmail.com>
2025-07-20 21:52:35 +0300

    код структурирован по разным классам и пакетам

--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -1,12 +1,13 @@
 package ru.stqa.geometry.figures;
 
 public class Square {
-    static void printSquareArea(double side) {      // для параметров нужно явное указание типа, var уже не подойдет
+    public static void printSquareArea(double side) {      // для параметров нужно явное указание типа, var уже не подойдет
                                                     // static - означает, что ф-ция глобальная
-        System.out.println("Площадь квадрата со стороной " + side + " = " + getSquareArea(side));
+        String text = String.format("Площадь квадрата со стороной %f = %f", side, getSquareArea(side));
+        System.out.println(text);
     }
 
-    private static double getSquareArea(double side) {
+    public static double getSquareArea(double side) {
         return side * side;
     }
 }
