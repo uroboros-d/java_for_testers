58533dd2ffa972c813a3479a50eddf8e44769b3e
Dzmitry Shablinski <tassap2002@gmail.com>
2025-07-20 22:58:06 +0300

    создан тест ф-ции расчета площади квадрата

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -2,7 +2,9 @@ package ru.stqa.geometry.figures;
 
 public class Rectangle {
     public static void printRectangleArea(double firstSide, double secondSide) {
-        System.out.println("Площадь прямоугольника со сторонами " + firstSide + " и " + secondSide + " = " + getRectangleArea(firstSide, secondSide));
+        String text = String.format("Площадь прямоугольника со сторонами %f и %f = %f",
+                firstSide, secondSide, getRectangleArea(firstSide, secondSide));
+        System.out.println(text);
     }
 
     public static double getRectangleArea(double firstSide, double secondSide) {
