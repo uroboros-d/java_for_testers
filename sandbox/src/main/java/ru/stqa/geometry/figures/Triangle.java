10dd05fa22359c905966bcb34c7270f4892f930a
Dzmitry Shablinski <tassap2002@gmail.com>
2025-07-24 22:36:05 +0300

    создан класс Triangle и тесты для функций вычисления периметра и площади треугольника

---
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -0,0 +1,14 @@
+package ru.stqa.geometry.figures;
+
+public record Triangle(double sideOne, double sideTwo, double sideThree) {
+
+    public double getPerimeter() {
+        return this.sideOne + this.sideTwo + this.sideThree;
+    }
+
+    public double getArea() {
+        var semiperimeter = (this.sideOne + this.sideTwo + this.sideThree) * 0.5;
+        var area = Math.sqrt(semiperimeter*(semiperimeter-sideOne)*(semiperimeter-sideTwo)*(semiperimeter-sideThree));
+        return area;
+    }
+}
--- sandbox/src/test/java/ru/stqa/geometry/figures/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/SquareTests.java
@@ -34,4 +34,19 @@ public class SquareTests {
     void canCalculateRectanglePerimeter() {
         Assertions.assertEquals(16., new Rectangle(5.,3).getPerimeter());
     }
+
+    @Test
+        // тест для вычисления площади
+    void canCalculateTriangleArea() {
+        var triangle = new Triangle(3., 4, 5.0);
+        var actResult = triangle.getArea();
+        var expResult = 6.;
+        Assertions.assertEquals(expResult, actResult);
+    }
+
+    @Test
+        // тест для вычисления периметра
+    void canCalculateTrianglePerimeter() {
+        Assertions.assertEquals(12., new Triangle(3.,4, 5.0).getPerimeter());
+    }
 }
