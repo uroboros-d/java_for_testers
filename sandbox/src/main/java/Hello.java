0c9c8d77cef731cd390bd7a18a7fe90dc0c0fd78
Dzmitry Shablinski <tassap2002@gmail.com>
2025-07-24 21:35:21 +0300

    исправил конфликты окончательно. всё работает хорошо

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,9 +1,7 @@
 import java.io.File;
 
-<<<<<<< HEAD
-import java.io.File;
-
 public class Hello {
+
     public static void main(String[] args) {
 
         // всё, что ниже, использовалось просто показать, что бывают объекты и их методы, вызывающиеся через точку
@@ -16,14 +14,5 @@ public class Hello {
 
         System.out.println(configFile.getAbsolutePath());
         // получить инф об абсолютном пути до объекта
-=======
-public class Hello {
-    public static void main(String[] args) {
-        //System.out.print("Hello, world!");
-
-        var configFile = new File("sandbox/build.gradle");  //создать объект файл с указание модуля, где он находится
-        System.out.println(configFile.exists());    // проверить существование объекта класса File с пар-ром pathname = "sandbox/build.gradle"
-        System.out.println(configFile.getAbsolutePath());   // получить инф об абсолютном пути до объекта
->>>>>>> origin/main
     }
 }
