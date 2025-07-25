d4273c542a129d0b3d779aab857892d744a5b67d
Dzmitry Shablinski <tassap2002@gmail.com>
2025-07-25 17:22:36 +0300

    изменил класс Hello и настройки для деления на ноль

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -4,15 +4,10 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        // всё, что ниже, использовалось просто показать, что бывают объекты и их методы, вызывающиеся через точку
+        var x = 1;
+        var y = 0;
+        var z = x / y;
 
-        var configFile = new File("sandbox/build.gradle");
-        // создать объект файл с указанием модуля, где он находится
-
-        System.out.println(configFile.exists());
-        // проверить существование объекта класса File с пар-ром pathname = "sandbox/build.gradle"
-
-        System.out.println(configFile.getAbsolutePath());
-        // получить инф об абсолютном пути до объекта
+        System.out.println(z);
     }
 }
