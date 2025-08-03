5349d15f576e8056a6f92c433a7fa2cf66da6f89
Dzmitry Shablinski <tassap2002@gmail.com>
2025-08-03 21:04:54 +0300

    еще маленький коммит

--- addressbook_web_tests/src/test/java/model/GroupData.java
+++ addressbook_web_tests/src/test/java/model/GroupData.java
@@ -1,4 +1,9 @@
 package model;
 
 public record GroupData(String name, String header, String footer) {
+    // явно указываем второй конструктор (первый заложен вверху в record)
+    public GroupData() {
+        // создаем объект, вызывая тот конструктор, который создается в record по дефолту
+        this("","","");
+    }
 }
