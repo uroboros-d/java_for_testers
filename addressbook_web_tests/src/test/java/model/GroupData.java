ba41a629592527c6208bf383c5a3912a4e1fc1e4
Dzmitry Shablinski <tassap2002@gmail.com>
2025-08-03 20:46:38 +0300

    сделал пустой конструктор GroupData

--- addressbook_web_tests/src/test/java/model/GroupData.java
+++ addressbook_web_tests/src/test/java/model/GroupData.java
@@ -1,4 +1,10 @@
 package model;
 
-public record GroupData(String group_two, String group_two_header, String group_two_footer) {
+public record GroupData(String name, String header, String footer) {
+
+    // явно указываем второй конструктор (первый заложен вверху в record)
+    public GroupData() {
+        // создаем объект, вызывая тот конструктор, который создается в record по дефолту
+        this("","","");
+    }
 }
