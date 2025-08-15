a0d19a18cebe6e89bc14af31fe107559688a60c4
Dzmitry Shablinski <tassap2002@gmail.com>
2025-08-15 13:53:30 +0300

    разрядились наушники

--- addressbook_web_tests/src/test/java/manager/GroupHelper.java
+++ addressbook_web_tests/src/test/java/manager/GroupHelper.java
@@ -24,21 +24,58 @@ public class GroupHelper {
 
     public void createGroup(GroupData group) {
         openGroupsPage();
+        initGroupCreation();
+        fillGroupForm(group);
+        submitGroupCreation();
+        returnToGroupsPage();
+    }
+
+    private void submitGroupCreation() {
+        manager.driver.findElement(By.name("submit")).click();
+    }
+
+    private void initGroupCreation() {
         manager.driver.findElement(By.name("new")).click();
+    }
+
+    public void removeGroup() {
+        openGroupsPage();
+        selectGroup();
+        manager.driver.findElement(By.name("delete")).click();
+        returnToGroupsPage();
+    }
+
+    public void modifyGroup(GroupData modifiedGroup) {
+        openGroupsPage();               //открыть страницу со списком групп
+        selectGroup();                  //выбрать группу - отметить ее галочкой
+        initGroupModification();        //нажать кнопку модификации
+        fillGroupForm(modifiedGroup);   //заполнить форму данными из объекта. переданного в кач-ве параметра в метод
+        submitGroupModification();      //нажать кнопку подтверждения изменения
+        returnToGroupsPage();           //вернуться на страницу со списком групп
+    }
+
+    private void returnToGroupsPage() {
+        manager.driver.findElement(By.linkText("group page")).click();
+    }
+
+    private void submitGroupModification() {
+        manager.driver.findElement(By.name("update")).click();
+    }
+
+    private void fillGroupForm(GroupData group) {
         manager.driver.findElement(By.name("group_name")).click();
         manager.driver.findElement(By.name("group_name")).sendKeys(group.name());
         manager.driver.findElement(By.name("group_header")).click();
         manager.driver.findElement(By.name("group_header")).sendKeys(group.header());
         manager.driver.findElement(By.name("group_footer")).click();
         manager.driver.findElement(By.name("group_footer")).sendKeys(group.footer());
-        manager.driver.findElement(By.name("submit")).click();
-        manager.driver.findElement(By.linkText("group page")).click();
     }
 
-    public void removeGroup() {
-        openGroupsPage();
+    private void initGroupModification() {
+        manager.driver.findElement(By.name("edit")).click();
+    }
+
+    private void selectGroup() {
         manager.driver.findElement(By.name("selected[]")).click();
-        manager.driver.findElement(By.name("delete")).click();
-        manager.driver.findElement(By.linkText("groups")).click();
     }
 }
