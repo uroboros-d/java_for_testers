5035f40e24d88e84f6699d3826682599379ce130
Dzmitry Shablinski <tassap2002@gmail.com>
2025-08-17 09:28:41 +0300

    метод canCreateContact без проверки страницы по элементу

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -6,44 +6,41 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.Dimension;
-import org.openqa.selenium.JavascriptExecutor;
 
-import java.util.*;
+import java.util.NoSuchElementException;
 
 public class ContactCreationTests {
-    //
-    private static WebDriver driver1;
-    private Map<String, Object> vars;
-    JavascriptExecutor js;
+    //объявление переменной, с помощью которой будет зарускаться браузер
+    private static WebDriver driver;
 
     @BeforeEach
     // перед тестом нужно запустить браузер, используя соответствующий драйвер
     public void setUp() {
-        if(driver1 == null) {               //проверка предусловий тестов
-            driver1 = new FirefoxDriver();
-            Runtime.getRuntime().addShutdownHook(new Thread(driver1::quit));
-            driver1.get("http://localhost/addressbook/");
-            driver1.manage().window().setSize(new Dimension(862, 680));
-            driver1.findElement(By.name("user")).sendKeys("admin");
-            driver1.findElement(By.name("pass")).sendKeys("secret");
-            driver1.findElement(By.xpath("//input[@value=\'Login\']")).click();
+        if(driver == null) {               //проверка предусловий тестов
+            driver = new FirefoxDriver();
+//            Runtime.getRuntime().addShutdownHook(new Thread(driver1::quit));
+            driver.get("http://localhost/addressbook/");
+            driver.manage().window().setSize(new Dimension(862, 680));
+            driver.findElement(By.name("user")).sendKeys("admin");
+            driver.findElement(By.name("pass")).sendKeys("secret");
+            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
         }
     }
 
     @Test
     public void canCreateContact() {
-        driver1.findElement(By.linkText("add new")).click();
-        driver1.findElement(By.name("firstname")).sendKeys("dz");
-        driver1.findElement(By.name("lastname")).sendKeys("dz");
-        driver1.findElement(By.name("mobile")).sendKeys("123");
-        driver1.findElement(By.xpath("//input[@value=\'Enter\']")).click();
-        driver1.findElement(By.linkText("home page")).click();
+        driver.findElement(By.linkText("add new")).click();
+        driver.findElement(By.name("firstname")).sendKeys("dz");
+        driver.findElement(By.name("lastname")).sendKeys("dz");
+        driver.findElement(By.name("mobile")).sendKeys("123");
+        driver.findElement(By.name("submit")).click();
+        driver.findElement(By.linkText("home page")).click();
     }
 
-    @Test
+        @Test
     public void canCreateContactWithAllEmptyProperties() {
-        driver1.findElement(By.linkText("add new")).click();
-        driver1.findElement(By.xpath("//input[@value=\'Enter\']")).click();
-        driver1.findElement(By.linkText("home page")).click();
+        driver.findElement(By.linkText("add new")).click();
+        driver.findElement(By.xpath("//input[@value=\'Enter\']")).click();
+        driver.findElement(By.linkText("home page")).click();
     }
 }
