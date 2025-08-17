95e0476558603a77b512f7529455c08538e115ba
Dzmitry Shablinski <tassap2002@gmail.com>
2025-08-17 18:36:33 +0300

    перенес driver1, setUp1, isElementPresent1 в TestBase

--- addressbook_web_tests/src/test/java/tests/ContactRemovalTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactRemovalTests.java
@@ -1,61 +1,29 @@
 package tests;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-import java.util.*;
-
-public class ContactRemovalTests {
-    //объявление переменной, с помощью которой будет запускаться браузер
-    private WebDriver driver;
-
-    @BeforeEach
-    // перед тестом нужно запустить браузер, используя соответствующий драйвер
-    public void setUp() {
-        if (driver == null) {               //проверка предусловий тестов
-            driver = new FirefoxDriver();
-            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/");
-            driver.manage().window().setSize(new Dimension(862, 680));
-            driver.findElement(By.name("user")).sendKeys("admin");
-            driver.findElement(By.name("pass")).sendKeys("secret");
-            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
-        }
-    }
+public class ContactRemovalTests extends TestBase {
 
     @Test
     public void canRemoveContact() {
         //если на текущей странице нет кнопки Delete,
         // то перейти в раздел home
-        if (! isElementPresent(By.cssSelector("input[value='Delete']"))) {
-            driver.findElement(By.linkText("home")).click();
+        if (! isElementPresent1(By.cssSelector("input[value='Delete']"))) {
+            driver1.findElement(By.linkText("home")).click();
         }
         //если нет ни одного контакта, т.е. нет элемента selected[]
         // - чекбокс перед контактом, то создать контакт
-        if(! isElementPresent(By.name("selected[]"))) {
-            driver.findElement(By.linkText("add new")).click();
-            driver.findElement(By.name("firstname")).sendKeys("dz");
-            driver.findElement(By.name("lastname")).sendKeys("dz");
-            driver.findElement(By.name("mobile")).sendKeys("123");
-            driver.findElement(By.name("submit")).click();
-            driver.findElement(By.linkText("home page")).click();
+        if(! isElementPresent1(By.name("selected[]"))) {
+            driver1.findElement(By.linkText("add new")).click();
+            driver1.findElement(By.name("firstname")).sendKeys("dz");
+            driver1.findElement(By.name("lastname")).sendKeys("dz");
+            driver1.findElement(By.name("mobile")).sendKeys("123");
+            driver1.findElement(By.name("submit")).click();
+            driver1.findElement(By.linkText("home page")).click();
         }
-        driver.findElement(By.name("selected[]")).click();
-        driver.findElement(By.cssSelector("input[value='Delete']")).click();
-    }
-
-    private boolean isElementPresent(By locator) {
-        try {
-            driver.findElement(locator);
-            return true;
-        } catch (NoSuchElementException exception) {
-            return false;
-        }
+        driver1.findElement(By.name("selected[]")).click();
+        driver1.findElement(By.cssSelector("input[value='Delete']")).click();
     }
 }
 
--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -2,12 +2,19 @@ package tests;
 
 import manager.ApplicationManager;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 //общий базовый класс для всех тестов для хранения повторяющегося кода
 public class TestBase {
 
     //объявление ссылки на ApplicationManager - мереджер управления приложением
     protected static ApplicationManager app;
+    //объявление переменной, с помощью которой будет запускаться браузер
+    protected WebDriver driver1;
 
     @BeforeEach
     // в setUp выполняются предусловия
@@ -18,5 +25,28 @@ public class TestBase {
         }
         //приложение запускать в браузере, указанном в browser, или в дефолтном firefox
         app.init(System.getProperty("browser", "firefox"));
+    }
+
+    @BeforeEach
+    // перед тестом нужно запустить браузер, используя соответствующий драйвер
+    public void setUp1() {
+        if (driver1 == null) {               //проверка предусловий тестов
+            driver1 = new FirefoxDriver();
+            Runtime.getRuntime().addShutdownHook(new Thread(driver1::quit));
+            driver1.get("http://localhost/addressbook/");
+            driver1.manage().window().setSize(new Dimension(862, 680));
+            driver1.findElement(By.name("user")).sendKeys("admin");
+            driver1.findElement(By.name("pass")).sendKeys("secret");
+            driver1.findElement(By.xpath("//input[@value=\'Login\']")).click();
+        }
+    }
+
+    protected boolean isElementPresent1(By locator) {
+        try {
+            driver1.findElement(locator);
+            return true;
+        } catch (NoSuchElementException exception) {
+            return false;
+        }
     }
 }
