1fc9f8c66745ec00f2b61431a101efa6b3a4116b
Dzmitry Shablinski <tassap2002@gmail.com>
2025-08-18 08:51:58 +0300

    перенес driver1, setUp1, isElementPresent1 в TestBase

--- addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/ContactCreationTests.java
@@ -1,59 +1,29 @@
 package tests;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.Dimension;
 
-import org.openqa.selenium.NoSuchElementException;
-
-public class ContactCreationTests {
-    //объявление переменной, с помощью которой будет запускаться браузер
-    private static WebDriver driver;
-
-    @BeforeEach
-    // перед тестом нужно запустить браузер, используя соответствующий драйвер
-    public void setUp() {
-        if (driver == null) {               //проверка предусловий тестов
-            driver = new FirefoxDriver();
-            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
-            driver.get("http://localhost/addressbook/");
-            driver.manage().window().setSize(new Dimension(862, 680));
-            driver.findElement(By.name("user")).sendKeys("admin");
-            driver.findElement(By.name("pass")).sendKeys("secret");
-            driver.findElement(By.xpath("//input[@value=\'Login\']")).click();
-        }
-    }
+public class ContactCreationTests extends TestBase{
 
     @Test
     public void canCreateContact() {
-        if (! isElementPresent(By.name("lastname"))) {
-            driver.findElement(By.linkText("add new")).click();
+        if (! isElementPresent1(By.name("lastname"))) {
+            driver1.findElement(By.linkText("add new")).click();
         }
-        driver.findElement(By.name("firstname")).sendKeys("dz");
-        driver.findElement(By.name("lastname")).sendKeys("dz");
-        driver.findElement(By.name("mobile")).sendKeys("123");
-        driver.findElement(By.name("submit")).click();
-        driver.findElement(By.linkText("home page")).click();
+        driver1.findElement(By.name("firstname")).sendKeys("dz");
+        driver1.findElement(By.name("lastname")).sendKeys("dz");
+        driver1.findElement(By.name("mobile")).sendKeys("123");
+        driver1.findElement(By.name("submit")).click();
+        driver1.findElement(By.linkText("home page")).click();
     }
 
     @Test
     public void canCreateContactWithAllEmptyProperties() {
-        if (! isElementPresent(By.name("lastname"))) {
-            driver.findElement(By.linkText("add new")).click();
+        if (! isElementPresent1(By.name("lastname"))) {
+            driver1.findElement(By.linkText("add new")).click();
         }
-        driver.findElement(By.xpath("//input[@value=\'Enter\']")).click();
-        driver.findElement(By.linkText("home page")).click();
+        driver1.findElement(By.xpath("//input[@value=\'Enter\']")).click();
+        driver1.findElement(By.linkText("home page")).click();
     }
 
-    private boolean isElementPresent(By locator) {
-        try {
-            driver.findElement(locator);
-            return true;
-        } catch (NoSuchElementException exception) {
-            return false;
-        }
-    }
 }
