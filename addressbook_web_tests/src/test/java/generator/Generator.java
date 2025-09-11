7db5e69215c4a6573bccf00dfc230ea208540d48
Dzmitry Shablinski <tassap2002@gmail.com>
2025-09-11 16:27:09 +0300

    сделал groups.xml

--- addressbook_web_tests/src/test/java/generator/Generator.java
+++ addressbook_web_tests/src/test/java/generator/Generator.java
@@ -4,6 +4,7 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import common.commonFunctions;
 import model.Group;
@@ -65,7 +66,12 @@ public class Generator {
         if ("yaml".equals(format)) {
             ObjectMapper mapper = new YAMLMapper();
             mapper.writeValue(new File(output), data);
-        } else {
+        }
+        if ("xml".equals(format)) {
+            ObjectMapper mapper = new XmlMapper();
+            mapper.writeValue(new File(output), data);
+        }
+        else {
             throw new IllegalArgumentException("Неизвестный формат данных " + format);
         }
     }
--- addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
+++ addressbook_web_tests/src/test/java/tests/GroupCreationTests.java
@@ -2,12 +2,14 @@ package tests;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import model.Group;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -31,22 +33,20 @@ public class GroupCreationTests extends TestBase {
 //                }
 //            }
 //        }
-        var json = "";
-        try (
-                var reader = new FileReader("groups.json");
-                var breader = new BufferedReader(reader);
-        ){
-            var line = breader.readLine();
-            while (line != null) {
-                json = json + line;
-                line = breader.readLine();
-            }
-        }
+//        var json = "";
+//        try (
+//                var reader = new FileReader("groups.json");
+//                var breader = new BufferedReader(reader);
+//        ){
+//            var line = breader.readLine();
+//            while (line != null) {
+//                json = json + line;
+//                line = breader.readLine();
+//            }
+//        }
 
-
-        //var json = Files.readString(Paths.get("groups.json"));
-        ObjectMapper mapper = new ObjectMapper();
-        var value = mapper.readValue(json,  new TypeReference<List<Group>>(){});
+        var mapper = new XmlMapper();
+        var value = mapper.readValue(new File("groups.xml"),  new TypeReference<List<Group>>(){});
         result.addAll(value);
         return result;
     }
