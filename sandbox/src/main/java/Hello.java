abe3ddee9f1bc1d7e9b6ca5a5a4fffe0e7e107e9
Dzmitry Shablinski <tassap2002@gmail.com>
2025-09-14 13:32:00 +0300

    начало

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,15 +1,21 @@
+import java.io.File;
+
 public class Hello {
 
     public static void main(String[] args) {
 
-            var x = 2;
-            var y = 0;
-            if (y==0) {
-                System.out.println("Division by zero is not allowed");
-            } else {
-                var z = divide(x, y);
-                System.out.println("Hello, world!");
-            }
+        var configFile = new File("sandbox/build.gradle");
+        System.out.println(configFile.getAbsolutePath());
+        System.out.println(configFile.exists());
+
+        var x = 2;
+        var y = 0;
+        if (y == 0) {
+            System.out.println("Division by zero is not allowed");
+        } else {
+            var z = divide(x, y);
+            System.out.println("Hello, world!");
+        }
     }
 
     private static int divide(int x, int y) {
