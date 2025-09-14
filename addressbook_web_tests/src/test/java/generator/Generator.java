b1021e0deab7faf721aa5aa113989531865b1f3b
Dzmitry Shablinski <tassap2002@gmail.com>
2025-09-14 15:50:21 +0300

    5.3 создал класс Generator и ввел опции командн строки для групп

---
+++ addressbook_web_tests/src/test/java/generator/Generator.java
@@ -0,0 +1,19 @@
+package generator;
+
+public class Generator {
+    public static void main(String[] args) {
+        new Generator().run();
+    }
+
+    private void run() {
+        var data = generate();
+        save(data);
+    }
+
+    private void save(Object data) {
+    }
+
+    private Object generate() {
+        return null;
+    }
+}
