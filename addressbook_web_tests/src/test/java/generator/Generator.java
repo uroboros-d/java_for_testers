bf6578ef825ac5262caa2464628b3292cf0e9059
Dzmitry Shablinski <tassap2002@gmail.com>
2025-09-14 16:18:32 +0300

    5.3 подключил библ JCommander и переделал класс Generator

--- addressbook_web_tests/src/test/java/generator/Generator.java
+++ addressbook_web_tests/src/test/java/generator/Generator.java
@@ -1,9 +1,38 @@
 package generator;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
 public class Generator {
+    //объявление набора св-тв, которые соотв-ют параметрам запуска
+    //св-во описывает тип генерируемых значений
+    //можно будет использовать длинную запись или короткую при испльзовании этого параметра
+    @Parameter(names={"--type", "-t"})
+    String type;
+
+    //св-во описывает название файла значений
+    @Parameter(names={"--output", "-o"})
+    String output;
+
+    // св-во описывает формат генерируемых значений
+    @Parameter(names={"--format", "-f"})
+    String format;
+
+    // св-во описывает кол-во генерируемых объектов
+    @Parameter(names={"--count", "-c"})
+    int count;
+
     public static void main(String[] args) {
-        new Generator().run();
+        var generator = new Generator();
+        //считывает пар-ры ком строки из args и помещает в объект generator
+        JCommander.newBuilder()
+                .addObject(generator)
+                .build()
+                .parse(args);
+        //генерир-ся объекты
+        generator.run();
     }
+
 
     private void run() {
         var data = generate();
