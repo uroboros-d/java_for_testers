5fb6fd9d36a962f04dd2ddbb9733d90db1549ef8
Dzmitry Shablinski <tassap2002@gmail.com>
2025-09-16 21:47:59 +0300

    на середине convertList

--- addressbook_web_tests/src/test/java/manager/HibernateHelper.java
+++ addressbook_web_tests/src/test/java/manager/HibernateHelper.java
@@ -1,7 +1,12 @@
 package manager;
+import manager.hbm.GroupRecord;
+import model.Group;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HibernateHelper extends HelperBase {
 
@@ -14,12 +19,26 @@ public class HibernateHelper extends HelperBase {
 
         sessionFactory = new Configuration()
 //                        .addAnnotatedClass(Book.class)
-//                        .addAnnotatedClass(Author.class)
+                        .addAnnotatedClass(GroupRecord.class)
                 .setProperty(AvailableSettings.JAKARTA_JDBC_URL,
                         "jdbc:mysql://localhost/addressbook")
                 // Credentials
                 .setProperty(AvailableSettings.JAKARTA_JDBC_USER, "root")
                 .setProperty(AvailableSettings.JAKARTA_JDBC_PASSWORD, "")
                 .buildSessionFactory();
+    }
+
+    //преобразует список объектов GroupRecord в список объектов Group
+    static List<Group> convertList(List<GroupRecord> records) {
+        List<Group> result = new ArrayList<>();
+        for (var record : records) {
+            result.add(new Group("" + record.id, record.name, record.header, record.footer));
+        }
+    }
+
+    public List<Group> getGroupList() {
+        return sessionFactory.fromSession(session -> {
+            return session.createQuery("from GroupRecord", GroupRecord.class).list();
+        });
     }
 }
